6f26f0b94b8d5d72f34885780861b8beb442c35d
runner404 <126064899+runner404@users.noreply.github.com>
2023-03-10 19:43:53 +0800

    自动化生成邮箱并注册vpn
    除验证码环节外，其他环节均实现自动化

--- outlook_register.py
+++ outlook_register.py
@@ -23,7 +23,7 @@ username.send_keys(first_line)
 next_button = driver.find_element("id","iSignupAction")
 next_button.click()
 
-time.sleep(1)
+time.sleep(2)
 
 # 输入密码
 password = driver.find_element("css selector","#PasswordInput")
@@ -65,11 +65,56 @@ year.send_keys("1990")
 next_button = driver.find_element("id","iSignupAction")
 next_button.click()
 
-time.sleep(30)
+time.sleep(10)
+print("已注册邮箱：",first_line)
+confirm = input("请完成oulook邮箱注册验证码环节后开始注册vpn\n准备好注册vpn了吗?(是或否)\n")
 
-# 点击“下一步”按钮
-next_button1 = driver.find_element("xpath","/html/body/div/div/div[1]/button")
-next_button1.click()
 
-print("已注册邮箱",first_line)
+def auto_rig(filename):
+    # 创建 Firefox 浏览器驱动
+    driver = webdriver.Firefox()
+
+    # 打开 Outlook 注册页面
+    driver.get("https://ababybus.com/index.php#/register")
+
+    select_element = driver.find_element("css selector","select.form-control")
+    select = Select(select_element)
+    select.select_by_index(4)
+
+    username = driver.find_element("css selector","div.form-group:nth-child(1) > input:nth-child(1)")
+
+    # 读取文本文件第一行
+    desktop_path = os.path.join(os.path.expanduser("~"), "Desktop")
+    #filename = os.path.join(desktop_path, "outlook_email.txt")
+    with open(filename, "r") as f:
+        first_line = f.readline().strip()  # 读取第一行并去除换行符
+    email_prefix = first_line.split("@")[0]  # 获取邮件地址前缀
+    username.send_keys(email_prefix)
+
+    password = driver.find_element("css selector","div.form-group:nth-child(3) > input:nth-child(1)")
+    password.send_keys("hello2023")
+
+    password_confirm = driver.find_element("css selector","div.form-group:nth-child(4) > input:nth-child(1)")
+    password_confirm.send_keys("hello2023")
+
+    next_button = driver.find_element("css selector",".btn-primary")
+    next_button.click()
+    return 1
+
+if confirm == "yes" or confirm == "是":
+    # 执行后续代码
+    print("Continuing...")
+    auto_rig(filename)
+    driver.get("https://outlook.live.com/mail/0/junkemail")
+    #删除已注册邮箱
+    with open(filename, "r+") as f:
+        data = f.readlines()
+        f.seek(0)  # 将文件指针移动到开头
+        f.writelines(data[1:])  # 写入除第一行之外的数据
+        f.truncate()  # 截断文件，删除最后一行可能存在的多余内容
+    print("已删除")
+else:
+    # 不执行后续代码
+    print("注册已中断，请重新开始")
+
 
