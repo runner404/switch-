2f373ce770635bca5717bbf8a0b7ba1039d18bf1
runner404 <126064899+runner404@users.noreply.github.com>
2023-03-10 19:40:44 +0800

    Update emial_generate.py
    重新更新了邮箱名生成文件，更加通俗易懂

--- emial_generate.py
+++ emial_generate.py
@@ -1,12 +1,18 @@
 import os
 
 # 批量生成邮箱号并保存到桌面
+
+print("请输入你想生成的邮箱名前缀，将根据输入的前缀生成“前缀_001”形式的邮箱名")
+print("例如输入chatgpt23，那么将生成chatgpt23_001@outlook.com-chatgpt23_999@outlook.com的邮箱名\n")
+email_prefix = input("请输入前缀：")
 desktop_path = os.path.join(os.path.expanduser("~"), "Desktop")
-filename = os.path.join(desktop_path, "outlook_email.txt")
+filename = os.path.join(desktop_path, "outlook_email11.txt")
 with open(filename, "w") as f:
-    for i in range(1, 1000):
-        email = "chatgpt111_{:03d}@outlook.com".format(i)
+    for i in range(5, 1000):
+        email = email_prefix + "_{:03d}@outlook.com".format(i)
         f.write(email + "\n")
 
 #邮箱名前缀"chatgpt111_"可自行更改，能注册就行
-#生成邮箱文本后，该文件即可删除
+#生成后，该文件即可删除
+
+print("邮箱列表文本outlook_email1.txt已生成")
